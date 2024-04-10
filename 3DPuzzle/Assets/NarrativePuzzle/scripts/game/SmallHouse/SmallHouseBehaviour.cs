21527369fed86b36855d82e795d9df0d7b78635b
Aieylisyh <Aieylisyh@users.noreply.github.com>
2024-04-10 17:27:11 +0800

    wip

---
+++ 3DPuzzle/Assets/NarrativePuzzle/scripts/game/SmallHouse/SmallHouseBehaviour.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SmallHouseBehaviour : MonoBehaviour
+{
+    public Transform mainCamera;
+    public Transform puzzleCamera;
+    public GameObject endBook;
+    public GameObject hud_enterPuzzle;
+    public void OnWatchBookShelf()
+    {
+
+    }
+
+    public void OnWatchEndBook()
+    {
+
+    }
+}
---
+++ 3DPuzzle/Assets/NarrativePuzzle/scripts/game/book/DraggableBook.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DraggableBook : MonoBehaviour
+{
+    public int idealOrder;
+    public int[] idealOrderReplacement;
+    public string finalString = "<size=150%>Y</size>";
+    private ParticleSystem _ps;
+    private TextMeshProUGUI _text;
+
+    private void Start()
+    {
+        _ps = GetComponentInChildren<ParticleSystem>();
+        _text = GetComponentInChildren<TextMeshProUGUI>();
+        if (_ps is null)
+            Debug.Log("DraggableBook no ps");
+        if (_text is null)
+            Debug.Log("DraggableBook no _text");
+    }
+
+    public void StartDrag()
+    {
+        _ps.Play();
+    }
+
+    public void EndDrag(Transform attach, bool isFinal)
+    {
+        _ps.Stop();
+        if (isFinal)
+        {
+            _text.text = finalString;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("OnMouseUp");
+    }
+
+    private void OnMouseDrag()
+    {
+        Debug.Log("OnMouseDrag");
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown");
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("OnMouseExit");
+    }
+}
