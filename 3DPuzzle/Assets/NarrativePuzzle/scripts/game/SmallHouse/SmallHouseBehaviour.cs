4d439078f8ce1338c62b33d3a5bbd000194369e1
Aieylisyh <Aieylisyh@users.noreply.github.com>
2024-04-10 17:48:26 +0800

    wip

--- 3DPuzzle/Assets/NarrativePuzzle/scripts/game/SmallHouse/SmallHouseBehaviour.cs
+++ 3DPuzzle/Assets/NarrativePuzzle/scripts/game/SmallHouse/SmallHouseBehaviour.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class SmallHouseBehaviour : MonoBehaviour
@@ -6,13 +7,43 @@ public class SmallHouseBehaviour : MonoBehaviour
     public Transform puzzleCamera;
     public GameObject endBook;
     public GameObject hud_enterPuzzle;
-    public void OnWatchBookShelf()
-    {
+    public GameObject hud_enterEndBook;
+    public float durationTransitCamera;
 
+    private void Start()
+    {
+        hud_enterPuzzle.SetActive(false);
+        hud_enterEndBook.SetActive(false);
+        endBook.SetActive(false);
+    }
+    public void ToggleWatchBookShelf(bool on)
+    {
+        Debug.Log("ToggleWatchBookShelf " + on);
+        hud_enterPuzzle.SetActive(on);
     }
 
-    public void OnWatchEndBook()
+    public void ToggleWatchEndBook(bool on)
     {
+        Debug.Log("ToggleWatchEndBook " + on);
+        hud_enterEndBook.SetActive(on);
+    }
 
+    public void EnterPuzzle()
+    {
+        Debug.Log("EnterPuzzle ");
+        //off player camera follow
+        //off player control
+        mainCamera.DOMove(puzzleCamera.position, durationTransitCamera).SetEase(Ease.InOutCubic);
+        mainCamera.DORotate(puzzleCamera.eulerAngles, durationTransitCamera).SetEase(Ease.InOutCubic);
+    }
+
+    public void OnPuzzleEnd()
+    {
+        //on player camera follow
+        //on player control
+        Debug.Log("OnPuzzleEnd ");
+        endBook.SetActive(true);
+        //show some chat
+        //play sound
     }
 }
