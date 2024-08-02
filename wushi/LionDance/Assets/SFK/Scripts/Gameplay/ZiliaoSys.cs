7a13e6c69678ab3ed4e836b7dabc0fbd49e7cf44
Aieylisyh <Aieylisyh@users.noreply.github.com>
2024-08-02 13:34:02 +0800

    wip

---
+++ wushi/LionDance/Assets/SFK/Scripts/Gameplay/ZiliaoSys.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+namespace Assets.SFK.Scripts.Gameplay
+{
+    public class ZiliaoSys : MonoBehaviour
+    {
+
+        public Transform cam;
+        public Transform pos1;
+        public Transform pos2;
+        public float duration = 2;
+        public GameObject nextLevel;
+
+        // Use this for initialization
+        void Start()
+        {
+            Move1();
+        }
+
+        void Move1()
+        {
+            cam.DOMove(pos1.position, duration).SetEase(Ease.InOutCubic).OnComplete(Move2);
+            cam.DORotate(pos1.eulerAngles, duration).SetEase(Ease.InOutCubic);
+        }
+        void Move2()
+        {
+            cam.DOMove(pos2.position, duration).SetEase(Ease.InOutCubic).OnComplete(Move1);
+            cam.DORotate(pos2.eulerAngles, duration).SetEase(Ease.InOutCubic);
+        }
+
+        private int countToggle = 0;
+
+        public void ToggleCanvasGroup(CanvasGroup cg)
+        {
+            cg.DOKill();
+            if (cg.alpha > 0)
+                cg.DOFade(0, 0.35f);
+            else
+                cg.DOFade(1, 0.35f);
+
+            CheckNextLevel();
+        }
+
+        void CheckNextLevel()
+        {
+            countToggle++;
+            if (countToggle >= 4)
+                nextLevel.SetActive(true);
+        }
+
+        public void OnClickNextLevel()
+        {
+            SceneManager.LoadScene("shilian 1");
+        }
+    }
+}
---
+++ wushi/LionDance/Assets/SFK/Scripts/com/Gameobject/UiCorrespondingWorldTransform.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace com
+{
+    public class UiCorrespondingWorldTransform : MonoBehaviour
+    {
+        [SerializeField] Camera _cam;
+        [SerializeField] Transform _host;
+        RectTransform _rect;
+        float _canvasScale;
+        [SerializeField] float _perferredWidth = 720;//2560
+        [SerializeField] float _perferredHeight = 1280;//1440
+        [SerializeField] Vector3 _offset;
+
+        void Start()
+        {
+            _rect = GetComponent<RectTransform>();
+            _rect.anchorMin = new Vector2(0, 0);
+            _rect.anchorMax = new Vector2(0, 0);
+
+            float w = Screen.width;
+            float h = Screen.height;
+
+            float ratio = w / h;
+            float perferredRatio = (float)_perferredWidth / _perferredHeight;
+
+            _canvasScale = (float)Screen.width / _perferredWidth;
+            if (ratio > perferredRatio)
+                _canvasScale *= perferredRatio / ratio;
+        }
+
+        void Update()
+        {
+            SyncPos();
+        }
+
+        void SyncPos()
+        {
+            var pos = Convert2DAnd3D.GetScreenPosition(_cam, _host.transform.position, _canvasScale);
+            _rect.anchoredPosition = pos + _offset;
+        }
+    }
+}
